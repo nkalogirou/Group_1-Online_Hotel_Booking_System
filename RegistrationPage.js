990b7743962b7dc6fbd1d06f67fc533a91568d8e
VasilisPanteli <vasilispanteli2003@gmail.com>
2025-11-01 15:22:02 +0200

    testing

---
+++ RegistrationPage.js
@@ -0,0 +1,22 @@
+import React from 'react'; //React components
+//will use bootstrap to make it easier trying to figure it out
+
+
+const Registration_Page = () => {
+
+return (
+
+<div> 
+  
+//title
+<h1> Registration Page </h1> 
+
+
+  
+  
+</div>
+)
+
+
+
+}
