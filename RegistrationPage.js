c384865613953eb71920702b5c71401bdcd4f52d
VasilisPanteli <vasilispanteli2003@gmail.com>
2025-11-03 22:05:26 +0200

    Registration page demo
    Updated the registration page layout to include a form for user input.

--- RegistrationPage.js
+++ RegistrationPage.js
@@ -6,14 +6,27 @@ const Registration_Page = () => {
 
 return (
 
-<div> 
-  
-//title
-<h1> Registration Page </h1> 
-
-
-  
-  
+<div className="card">
+  <div className="card-body">
+    <h5 className="card-title">Create Your Account</h5>
+    
+    <div className="form-floating mb-3">
+      <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+      <label htmlFor="floatingInput">Email address</label>
+    </div>
+    
+    <div className="form-floating mb-3">
+      <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+      <label htmlFor="floatingPassword">Password</label>
+    </div>
+    
+    <div className="form-floating mb-3">
+      <input type="password" className="form-control" id="floatingPasswordRepeat" placeholder="Repeat Password" />
+      <label htmlFor="floatingPasswordRepeat">Repeat Password</label>
+    </div>
+    
+    <button type="button" className="btn btn-primary w-100">Sign Up</button>
+  </div>
 </div>
 )
 
