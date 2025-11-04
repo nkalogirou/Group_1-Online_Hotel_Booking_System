22419828c7b6501000e8abea1ec1c9174ffebdd1
christosmouchlis21 <cm231339@students.euc.ac.cy>
2025-11-04 22:37:27 +0200

    Latest Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-// ======== Hotel Booking System - Server.js ========
-//"(test version)"
+// ======= Hotel Booking System – Server.js =======
+// (test version)
+
 // Εισαγωγή modules
 const express = require("express");
 const cors = require("cors");
-const app = express();
-const PORT = 3000;
-
-// Μεσαία λογισμικά (middlewares)
-app.use(cors());
-app.use(express.json());
-
 const authRoutes = require("./routes/auth");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Χρήση routes
 app.use("/auth", authRoutes);
 
 // Δοκιμαστικό route
 app.get("/", (req, res) => {
-  res.send("Server Working");
+    res.send("Server Working");
 });
 
 // Εκκίνηση server
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
 });
