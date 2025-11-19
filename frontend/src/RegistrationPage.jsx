7695f12ce6f5d250aa6231138e564b1634d3ca4c
VasilisPanteli <vasilispanteli2003@gmail.com>
2025-11-19 20:35:10 +0200

    updated App.jsx remove App.css and tested RegistrationPage trying to make container center

--- frontend/src/RegistrationPage.jsx
+++ frontend/src/RegistrationPage.jsx
@@ -1,20 +1,39 @@
+//Vasilis P.
 import React from 'react'; //React components
 //will use bootstrap to make it easier trying to figure it out
+//<h5 className="card-title">Create Your Account</h5>
 
+//const Registration_Page = () => 
+ 
+  
+//NOTES
+//used form-check-inline to made radio buttons horizontal
 
-const Registration_Page = () => {
+function RegistrationPage() {
 
 return (
 
-    <div className="container mt-4">
-      <div className="card">
-        <div className="card-body">
+    <div className="container">
+       <div className="card" >
+        <div className="card-body ">
           <h3 className="card-title">Create Your Account</h3>
 
-<div className="card">
-  <div className="card-body">
-    <h5 className="card-title">Create Your Account</h5>
+<div className="card ">
+  <div className="card-body ">
     
+<div className="form-check form-check-inline">
+  <input className="form-check-input form-check-inline" type="radio" name="radioDefault" id="radioDefault1" />
+  <label className="form-check-label" htmlFor="radioDefault1">
+    User
+  </label>
+</div>
+<div className="form-check form-check-inline">
+  <input className="form-check-input form-check-inline" type="radio" name="radioDefault" id="radioDefault2" defaultChecked />
+  <label className="form-check-label" htmlFor="radioDefault2">
+    Admin
+  </label>
+</div>
+
     <div className="form-floating mb-3">
       <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
       <label htmlFor="floatingInput">Email address</label>
@@ -42,4 +61,4 @@ return (
 
 
 };
-export default Registration_Page;
+export default RegistrationPage;
