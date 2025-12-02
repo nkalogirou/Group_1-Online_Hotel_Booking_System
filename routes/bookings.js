9516fa79fe4122f2d4a8819e32b49137f96cc069
christosmouchlis21 <cm231339@students.euc.ac.cy>
2025-12-02 17:33:26 +0200

    booking cancellation added
    status now changes to cancelled when cancelling a booking

--- routes/bookings.js
+++ routes/bookings.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const bookingController = require('../controllers/bookings');
 const authMiddleware = require('../authMiddleware');
 
+
 router.post('/', authMiddleware, bookingController.createBooking);
+
+router.post('/:id/cancel', authMiddleware, bookingController.cancelBooking);
 
 module.exports = router;
